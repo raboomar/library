6c1b6cc7c3914a75aac0ed4621e7a389997b2010
Rami <aboomar.2@osu.edu>
2022-08-16 16:12:27 -0400

    added book card and added css styling

--- library/src/components/bookList/BookList.jsx
+++ library/src/components/bookList/BookList.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import "./bookList.css";
 const BookList = () => {
-  return <div>BookList</div>;
+  let books = [
+    { title: "test1", author: "test1" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+    { title: "test2", author: "test2" },
+  ];
+  return (
+    <>
+      {books.map((book) => (
+        <div className="card">
+          <div className="container">
+            <h4 className="book-name">{book.title}</h4>
+            <h5 className="book-author">{book.author}</h5>
+            <button className="read-it"> Read</button>
+            <button className="remove-book">Remove</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default BookList;
