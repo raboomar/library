08c36ce8ae7c8ac560e4344fb8023d0062275c56
Rami <aboomar.2@osu.edu>
2022-08-15 20:17:26 -0400

    added book list

--- library/src/components/landingPage/LandingPage.jsx
+++ library/src/components/landingPage/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import BookList from "../bookList/BookList";
 import "./landingPage.css";
 const LandingPage = () => {
   return (
     <div className="landing-container">
       <div className="add-book-container">
         <button className="add-book-btn">Add New Book</button>
+      </div>
+      <div className="books">
+        <BookList />
       </div>
     </div>
   );
